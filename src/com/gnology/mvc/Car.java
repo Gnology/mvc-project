2763448f9a3cdec0fc17bfdf29a718dd5ff8c134
Damian Ujma <midosinid@yandex.ru>
2015-05-02 19:39:36 +0200

    new

---
+++ src/com/gnology/mvc/Car.java
@@ -0,0 +1,16 @@
+package com.gnology.mvc;
+
+public class Car {
+	private String name;
+	
+	public Car(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return name + " o dlugosci: " + name.length();
+	}
+	
+	
+}
--- src/com/gnology/mvc/MainController.java
+++ src/com/gnology/mvc/MainController.java
@@ -1,14 +1,21 @@
 package com.gnology.mvc;
 
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/test")
 public class MainController{
-	@RequestMapping("/test/model")
-	public String modelMethod(Model model){
-		model.addAttribute("message", "moja wiadomosc");
-		return "glowny";
+	
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	public String getMovie(@PathVariable String name, ModelMap model) {
+		model.addAttribute("message", new Car(name).toString());
+		return "main";
 	}
+	
+	
 }
